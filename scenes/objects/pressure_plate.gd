afa6f32277cd3a15e5771c724ee49b0ee5c6680f
JustMikkk <golaszevskymikolaj@gmail.com>
2024-10-13 10:04:16 +0200

    .

--- scenes/objects/pressure_plate.gd
+++ scenes/objects/pressure_plate.gd
@@ -21,9 +21,9 @@ var pressed: bool = false:
 		sprite_2d.texture = sprite_pressed if pressed else sprite
 		if pressed != val:
 			call_deferred("switch_spikes")
-			Shaker.shake(self, 20, 0.7)
+			Shaker.shake(self, 40, 1.7)
 			for spike in spikes:
-				Shaker.shake(spike, 20, 0.7)
+				Shaker.shake(spike, 40, 1.7)
 		pressed = val
 
 
